390d207c63bc36886d49d930fa1f4ba71cf1385d
zhangaj <zhangaj@x-teach.com>
2017-01-10 16:17:58 +0800

    add demension config

--- js/game.js
+++ js/game.js
@@ -14,34 +14,44 @@
 		init: function (options) {
 			this.options = options || {
 				chess_boarder: '',
-				result_wrapper: ''
+				result_wrapper: '',
+				dimension: 3
 			};
 			this.chess_boarder = $(this.options.chess_boarder);
 			this.chess_piece_wrapper = this.chess_boarder.find('li');
-			this.empty_piece_wrapper = this.chess_piece_wrapper;
 			this.result_wrapper = $(this.options.result_wrapper);
+			this.dimension = this.options.dimension;
+
 			for( var i = 0; i< this.chess_piece_wrapper.length; i++) {
-				this.chess_piece_wrapper[i].index = i;
+				this.chess_piece_wrapper[i].index_x = parseInt(i / this.dimension);
+				this.chess_piece_wrapper[i].index_y = parseInt(i % this.dimension);
 			}
 
 			var _self = this;
 			this.chess_piece_wrapper.on('click', function () {
+				var _this = this;
+				var n = _self.dimension;
 				_self._generate_piece(this,'first');
-				if(_self._check_if_win(this.index,'rp')) {
+
+				if(_self._check_if_win(this.index_x, this.index_y, n, 'rp')) {
 					return;
 				}
+
 				var next_target = _self._get_random_blcok();
 				setTimeout(function () {
 					_self._generate_piece(next_target, 'second');
-					if( _self._check_if_win(this.index,'sp')) {
+
+					if( _self._check_if_win(next_target.index_x, next_target.index_y, n, 'sp')) {
 						return;
 					}
+
 				}, 1000);
 
 			});
 		},
 
 		_generate_piece: function (element,player) {
+			
 			if (player == 'first') {
 				var temp = _RP_TPL;
 				$(element).addClass('rp');
@@ -55,83 +65,50 @@
 
 		_get_random_blcok: function () {
 			var empty_block_list =  this.chess_boarder.find('.empty');
+
 			if (empty_block_list.length !== 0) {
 				var x = parseInt(Math.random() * empty_block_list.length, 10);
 			}
+
 			return empty_block_list[x];
 		},
 
-		_check_if_win: function (x, player) {
-			
-			var result = false;
+		_check_if_win: function (x, y, n,player) {
+			console.log(n);
 
-			this.prev = function (x) {
-				return x - 1;
-			}
-			this.next = function (x) {
-				return x + 1;
-			}
-			this.above = function (x) {
-				return x - 3;
-			}
-			this.below = function (x) {
-				return x + 3;
-			}
-
-			this.check = function (x, player) {
-				return $(this.chess_piece_wrapper[x]).hasClass(player);
-			}
-
-			if( x % 3 === 1){
-				
-				if(this.check(this.prev(x), player) && this.check(this.next(x), player)) {
-					result = true;
-				}
-
-				if(parseInt(x / 3, 10) === 1 && this.check(this.above(x), player) && this.check(this.below(x), player)) {
-					result = true;
-				}
-
-				if(parseInt(x / 3, 10) === 1 && this.check(this.above(x) - 1, player) && this.check(this.below(x) + 1, player)) {
-					result = true;
-				}
-
-				if(parseInt(x / 3, 10) === 1 && this.check(this.above(x) + 1, player) && this.check(this.below(x) - 1, player)) {
-					result = true;
-				}
-
-				if(parseInt(x / 3, 10) !== 1 && this.check(4, player) && this.check(4 - (x - 4), player)){
-					result = true;
-				}
-
-			}else{
-
-				if( x % 3 === 0 && this.check(x + 1, player) && this.check(x + 2, player)){
-					result = true;
-				}
-
-				if( x % 3 === 0 && this.check(3, player) && this.check(3 - (x - 3), player)){
-					result = true;
-				}
-
-				if( x % 3 === 2 && this.check(x - 1, player) && this.check(x - 2, player)){
-					result = true;
-				}
-
-				if( x % 3 === 2 && this.check(5, player) && this.check(5 - (x - 5), player)){
-					result = true;
-				}
-
-				if( parseInt(x / 3, 10) === 1 && this.check(x + 3, player) && this.check(x - 3, player)){
-					result = true;
-				}
-
-				if( parseInt(x / 3, 10) !== 1 && this.check(4, player) && this.check(4 - (x - 4), player)){
-					result = true;
+			this.piece_martrix = new Array;
+			for(var i = 0; i < n; i++) {
+				this.piece_martrix[i] = new Array;
+				for(var j = 0; j < n; j++) {
+					this.piece_martrix[i][j] = $(this.chess_piece_wrapper[i*n + j]);
 				}
 			}
 
-			if(result) {
+			this.check = function (x,y, player) {
+
+				if( x >= 0 && x < n && y >= 0 && y < n ) {
+					return $(this.chess_piece_wrapper[x*n + y]).hasClass(player);
+				}else{
+					return false;
+				}
+			}
+
+			if( this.check(x-1,y, player) && this.check(x+1,y, player) || this.check(x-1,y, player) && this.check(x-2,y, player) || this.check(x+1,y, player) && this.check(x+2,y, player) ) {
+				this._end_game(player);
+				return true;
+			}
+
+			if( this.check(x,y-1, player) && this.check(x,y+1,player) || this.check(x,y-1,player) && this.check(x,y-2,player) || this.check(x,y+1, player) && this.check(x,y+2, player) ) {
+				this._end_game(player);
+				return true;
+			}
+
+			if( this.check(x-1,y+1, player) && this.check(x+1,y-1, player) || this.check(x-1,y+1, player) && this.check(x-2,y+2, player) || this.check(x+1,y-1, player) && this.check(x+2,y-2, player) ) {
+				this._end_game(player);
+				return true;
+			}
+
+			if( this.check(x-1,y-1,player) && this.check(x+1,y+1,player) || this.check(x-1,y-1, player) && this.check(x-2,y-2, player) || this.check(x+1,y+1, player) && this.check(x+2,y+2, player) ) {
 				this._end_game(player);
 				return true;
 			}
